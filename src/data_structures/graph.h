ac2895fa66eb636d615b475bd29dd265b5cd578c
ambikabish <ambikabisht605@gmail.com>
2025-10-24 21:52:48 +0530

    udated graph
    Updated GraphNode and GraphEdge structures to include traffic_factor. Modified function signatures for graph_add_node, graph_add_edge, and find_shortest_path to include new parameters. Added new functions for finding nearby locations and removing nodes and edges.

--- src/data_structures/graph.h
+++ src/data_structures/graph.h
@@ -6,29 +6,39 @@
 
 typedef struct GraphNode {
     int id;
-    char *name;
+    char *name;                 
     double latitude;
     double longitude;
-    LinkedList *neighbors;
+    LinkedList *neighbors;      
 } GraphNode;
 
 typedef struct GraphEdge {
     GraphNode *source;
     GraphNode *destination;
-    double distance;
-    double time;
+    double distance;           
+    double time;                
+    double traffic_factor;      
 } GraphEdge;
 
 typedef struct {
-    HashTable *nodes;
-    LinkedList *edges;
-    int next_node_id;
+    HashTable *nodes;           
+    LinkedList *edges;          
+    int next_node_id;           
 } Graph;
 
 Graph* create_graph();
-GraphNode* graph_add_node(Graph *graph, char *name, double lat, double lon);
-void graph_add_edge(Graph *graph, GraphNode *src, GraphNode *dest, double distance);
-LinkedList* find_shortest_path(Graph *graph, GraphNode *start, GraphNode *end);
+
+GraphNode* graph_add_node(Graph *graph, const char *name, double lat, double lon);
+
+void graph_add_edge(Graph *graph, GraphNode *src, GraphNode *dest, double distance, double traffic_factor);
+
+LinkedList* find_shortest_path(Graph *graph, GraphNode *start, GraphNode *end, int use_traffic_factor);
+
+LinkedList* find_nearby_locations(Graph *graph, double latitude, double longitude, double radius_km);
+
+void graph_remove_node(Graph *graph, GraphNode *node);
+void graph_remove_edge(Graph *graph, GraphNode *src, GraphNode *dest);
+
 void free_graph(Graph *graph);
 
-#endif
+#endif 
